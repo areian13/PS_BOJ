c104a8b8173577fa641c1a3d305b704c746a2a72
Areian <iantyrho13@naver.com>
2024-05-28 16:34:00 +0900

    Code Styling

--- 1. Bronze/Bronze 3/[B3] 04388 받아올림.cpp
+++ 1. Bronze/Bronze 3/[B3] 04388 받아올림.cpp
@@ -1,85 +1,45 @@
 #include <iostream>
-#include <vector>
-#include <array>
-#include <string>
-#include <time.h>
-#include <algorithm>
-#include <stdlib.h>
-#include <math.h>
-#include <cmath>
-#include <queue>
-#include <stack>
-#include <deque>
-#include <map>
-#include <unordered_map>
-#include <set>
-#include <limits.h>
-#include <string.h>
 
-#define Endl << "\n"
-#define endL << "\n" <<
-#define Cout cout <<
-#define	COUT cout << "OUT: " <<
-#define Cin cin >>
-#define fspc << " "
-#define spc << " " <<
-#define Enter cout << "\n"
-#define if if
-#define elif else if
-#define else else
-#define For(n) for(int i = 0; i < n; i++)
-#define Forj(n) for(int j = 0; j < n; j++)
-#define Foro(n) for(int i = 1; i <= n; i++)
-#define Forjo(n) for(int j = 1; j <= n; j++)
-#define between(small, middle, big) (small < middle && middle < big)
-#define among(small, middle, big) (small <= middle && middle <= big)
-#define stoe(container) container.begin(), container.end()
-#define lf(d) Cout fixed; cout.precision(d);
-#define ulf cout.unsetf(ios::scientific);
 #define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
-#define PI 3.14159265359
-
-typedef long long LLONG;
-typedef unsigned long long ULLONG;
-typedef unsigned int UINT;
 
 using namespace std;
 
-template <typename T>
-class heap : public priority_queue<T, vector<T>, greater<T>>
+int CountCarry(long long a, long long b)
 {
-};
+    int result = 0;
+    bool carry = false;
+    while (a > 0 || b > 0)
+    {
+        int l = a % 10;
+        int r = b % 10;
+
+        if (l + r + carry >= 10)
+        {
+            result++;
+            carry = true;
+        }
+        else
+            carry = false;
+
+        a /= 10;
+        b /= 10;
+    }
+    return result;
+}
 
 int main()
 {
-	FastIO;
+    FastIO;
 
-	while (true)
-	{
-		LLONG a, b;
-		Cin a >> b;
+    while (true)
+    {
+        long long a, b;
+        cin >> a >> b;
 
-		if (a == 0 && b == 0)
-			break;
+        if (a == 0 && b == 0)
+            break;
 
-		int result = 0;
-		bool carry = false;
-		while (a > 0 || b > 0)
-		{
-			int l = a % 10;
-			int r = b % 10;
-
-			if (l + r + carry >= 10)
-			{
-				result++;
-				carry = true;
-			}
-			else
-				carry = false;
-
-			a /= 10;
-			b /= 10;
-		}
-		Cout result Endl;
-	}
+        int result = CountCarry(a, b);
+        cout << result << '\n';
+    }
 }
