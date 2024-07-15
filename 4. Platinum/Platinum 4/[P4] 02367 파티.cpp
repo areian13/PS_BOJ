bdbfc76bcbb804cb7ff1abf55732a3ee2c3219a7
Areian <iantyrho13@naver.com>
2024-07-15 11:31:24 +0900

    PS

--- 4. Platinum/Platinum 4/[P4] 02367 파티.cpp
+++ 4. Platinum/Platinum 4/[P4] 02367 파티.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <climits>
+#include <queue>
 #include <algorithm>
 
 #define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
@@ -27,7 +27,7 @@ struct Edge
     }
 };
 
-int MaxFlow(int s, int t, vector<vector<Edge*>>& graph, bool onlyRoute = false)
+int MaxPlate(int s, int t, vector<vector<Edge*>>& graph)
 {
     int n = graph.size();
 
@@ -57,8 +57,6 @@ int MaxFlow(int s, int t, vector<vector<Edge*>>& graph, bool onlyRoute = false)
                 path[v] = edge;
                 Q.push(v);
 
-                if (onlyRoute && v == t)
-                    return -1;
                 if (v == t)
                     break;
             }
@@ -78,63 +76,63 @@ int MaxFlow(int s, int t, vector<vector<Edge*>>& graph, bool onlyRoute = false)
     return result;
 }
 
-int CountImportantEdge(int s, int t, vector<vector<Edge*>>& graph)
-{
-    int n = graph.size();
-
-    int flow = MaxFlow(s, t, graph);
-    int result = 0;
-
-    for (int u = 0; u < n; u++)
-    {
-        for (Edge* edge : graph[u])
-        {
-            if (edge->c == 0 || edge->Spare() != 0)
-                continue;
-
-            edge->c--;
-            edge->AddFLow(-1);
-            result += (MaxFlow(u, edge->v, graph, true) != -1);
-            edge->c++;
-            edge->AddFLow(+1);
-        }
-    }
-    return result;
-}
-
 int main()
 {
-    int tc;
-    cin >> tc;
+    int n, k, d;
+    cin >> n >> k >> d;
 
-    while (tc--)
+    vector<vector<Edge*>> graph(n * 2 + d + 2);
+    int s = 0;
+    int t = n * 2 + d + 1;
+    for (int u = n * 2 + 1; u <= n * 2 + d; u++)
     {
-        int n, m;
-        cin >> n >> m;
+        int c;
+        cin >> c;
 
-        vector<vector<Edge*>> graph(n);
-        for (int i = 0; i < m; i++)
+        Edge* e1 = new Edge(t, c);
+        Edge* e2 = new Edge(u, 0);
+        e1->rev = e2;
+        e2->rev = e1;
+        graph[u].push_back(e1);
+        graph[t].push_back(e2);
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        int u = i * 2 - 1;
+
+        Edge* e1 = new Edge(u, INT_MAX);
+        Edge* e2 = new Edge(s, 0);
+        e1->rev = e2;
+        e2->rev = e1;
+        graph[s].push_back(e1);
+        graph[u].push_back(e2);
+
+        e1 = new Edge(u + 1, k);
+        e2 = new Edge(u, 0);
+        e1->rev = e2;
+        e2->rev = e1;
+        graph[u].push_back(e1);
+        graph[u + 1].push_back(e2);
+
+        int z;
+        cin >> z;
+
+        for (int i = 0; i < z; i++)
         {
-            int u, v, c;
-            cin >> u >> v >> c;
-            u--, v--;
+            int v;
+            cin >> v;
+            v += n * 2;
 
-            Edge* e1 = new Edge(v, c);
-            Edge* e2 = new Edge(u, 0);
+            e1 = new Edge(v, 1);
+            e2 = new Edge(u + 1, 0);
             e1->rev = e2;
             e2->rev = e1;
-
-            graph[u].push_back(e1);
+            graph[u + 1].push_back(e1);
             graph[v].push_back(e2);
         }
-
-        int result = CountImportantEdge(0, n - 1, graph);
-        cout << result << '\n';
-
-        for (int u = 0; u < n; u++)
-        {
-            for (Edge* edge : graph[u])
-                delete edge;
-        }
     }
+
+    int result = MaxPlate(s, t, graph);
+    cout << result << '\n';
 }
