b5e95ba025069914b6fd18da293bcb1adacc6ed0
Areian <iantyrho13@naver.com>
2024-08-13 18:44:29 +0900

    Code Styling

--- 1. Bronze/Bronze 4/[B4] 26731 Zagubiona litera.cpp
+++ 1. Bronze/Bronze 4/[B4] 26731 Zagubiona litera.cpp
@@ -1,72 +1,32 @@
 #include <iostream>
-#include <vector>
-#include <array>
 #include <string>
-#include <time.h>
-#include <algorithm>
-#include <stdlib.h>
-#include <math.h>
-#include <cmath>
-#include <queue>
-#include <stack>
-#include <deque>
-#include <map>
-#include <unordered_map>
-#include <set>
-#include <limits.h>
-#include <float.h>
-#include <string.h>
+#include <array>
 
-#define Endl << "\n"
-#define endL << "\n" <<
-#define Cout cout <<
-#define	COUT cout << "OUT: " <<
-#define Cin cin >>
-#define fspc << " "
-#define spc << " " <<
-#define Enter cout << "\n"
-#define if if
-#define elif else if
-#define else else
-#define For(n) for(int i = 0; i < n; i++)
-#define Forj(n) for(int j = 0; j < n; j++)
-#define Foro(n) for(int i = 1; i <= n; i++)
-#define Forjo(n) for(int j = 1; j <= n; j++)
-#define between(small, middle, big) (small < middle && middle < big)
-#define among(small, middle, big) (small <= middle && middle <= big)
-#define stoe(container) container.begin(), container.end()
-#define lf(d) Cout fixed; cout.precision(d);
-#define ulf cout.unsetf(ios::scientific);
 #define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
-#define PI 3.14159265359
-
-typedef long long LLONG;
-typedef unsigned long long ULLONG;
-typedef unsigned int UINT;
 
 using namespace std;
 
-template <typename T>
-class heap : public priority_queue<T, vector<T>, greater<T>>
+char UnusedChar(string& str)
 {
-};
+    array<bool, 26> isUsed = { false, };
+    for (char c : str)
+        isUsed[c - 'A'] = true;
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (!isUsed[i])
+            return 'A' + i;
+    }
+    return '\0';
+}
 
 int main()
 {
-	FastIO;
+    FastIO;
 
-	string str;
-	Cin str;
+    string str;
+    cin >> str;
 
-	array<bool, 26> isUsed = { false, };
-	For(str.size())
-		isUsed[str[i] - 'A'] = true;
-	For(26)
-	{
-		if (!isUsed[i])
-		{
-			Cout(char)(i + 'A');
-			break;
-		}
-	}
+    char result = UnusedChar(str);
+    cout << result << '\n';
 }
--- 1. Bronze/Bronze 4/[B4] 26736 Wynik meczu.cpp
+++ 1. Bronze/Bronze 4/[B4] 26736 Wynik meczu.cpp
@@ -1,69 +1,24 @@
 #include <iostream>
-#include <vector>
-#include <array>
+#include <cstdio>
 #include <string>
-#include <time.h>
-#include <algorithm>
-#include <stdlib.h>
-#include <math.h>
-#include <cmath>
-#include <queue>
-#include <stack>
-#include <deque>
-#include <map>
-#include <unordered_map>
-#include <set>
-#include <limits.h>
-#include <float.h>
-#include <string.h>
 
-#define Endl << "\n"
-#define endL << "\n" <<
-#define Cout cout <<
-#define	COUT cout << "OUT: " <<
-#define Cin cin >>
-#define fspc << " "
-#define spc << " " <<
-#define Enter cout << "\n"
-#define if if
-#define elif else if
-#define else else
-#define For(n) for(int i = 0; i < n; i++)
-#define Forj(n) for(int j = 0; j < n; j++)
-#define Foro(n) for(int i = 1; i <= n; i++)
-#define Forjo(n) for(int j = 1; j <= n; j++)
-#define between(small, middle, big) (small < middle && middle < big)
-#define among(small, middle, big) (small <= middle && middle <= big)
-#define stoe(container) container.begin(), container.end()
-#define lf(d) Cout fixed; cout.precision(d);
-#define ulf cout.unsetf(ios::scientific);
 #define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
-#define PI 3.14159265359
-
-typedef long long LLONG;
-typedef unsigned long long ULLONG;
-typedef unsigned int UINT;
 
 using namespace std;
 
-template <typename T>
-class heap : public priority_queue<T, vector<T>, greater<T>>
-{
-};
-
 int main()
 {
-	FastIO;
+    FastIO;
 
-	string str;
-	Cin str;
+    string str;
+    cin >> str;
 
-	int a = 0;
-	int b = 0;
-	for (auto chr : str)
-	{
-		a += (chr == 'A');
-		b += (chr == 'B');
-	}
-	printf("%d : %d", a, b);
+    int a = 0;
+    int b = 0;
+    for (char c : str)
+    {
+        a += (c == 'A');
+        b += (c == 'B');
+    }
+    printf("%d : %d", a, b);
 }
