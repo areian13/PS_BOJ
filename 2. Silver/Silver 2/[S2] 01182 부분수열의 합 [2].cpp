9278eef16a4a044e92f4032777c5e448e5e212f0
Areian <iantyrho13@naver.com>
2025-07-22 16:56:35 +0900

    2025-07-22 16'56

---
+++ 2. Silver/Silver 2/[S2] 01182 부분수열의 합 [2].cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+
+using namespace std;
+
+int main()
+{
+    FastIO;
+
+    int n, s;
+    cin >> n >> s;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    int result = 0;
+    for (int i = 1; i < (1 << n); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (i & (1 << j))
+                sum += a[j];
+        }
+
+        result += (sum == s);
+    }
+    cout << result << '\n';
+}
