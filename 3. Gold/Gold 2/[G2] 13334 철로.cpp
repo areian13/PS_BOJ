69ee7c7dab24c414656acb078e292b2f527a7e97
areian13 <iantyrho13@naver.com>
2025-08-28 09:45:29 +0900

    2025-08-28 09'45

--- 3. Gold/Gold 2/[G2] 13334 철로.cpp
+++ 3. Gold/Gold 2/[G2] 13334 철로.cpp
@@ -20,37 +20,5 @@ int main()
     cin >> n;
 
     vector<Line> lines(n);
-    for (auto& [s, e] : lines)
-    {
-        cin >> s >> e;
-        if (s > e)
-            swap(s, e);
-    }
-    sort(lines.begin(), lines.end(),
-        [](const Line& a, const Line& b)
-        {
-            if (a.e != b.e)
-                return a.e < b.e;
-            return a.s < b.s;
-        }
-    );
-
-    int d;
-    cin >> d;
-
-    int result = 0;
-    int maxS = INT_MIN;
-    for (int l = 0, r = 0; r < n;)
-    {
-        if (maxS >= lines[r].e - d)
-        {
-            result = max(result, r - l + 1);
-            r++;
-        }
-        else
-            l++;
-
-        r += (l >= r);
-    }
-    cout << result << '\n';
+    
 }
