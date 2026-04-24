7e752190a1e754730205994255975d2e5513b77c
areian13 <iantyrho13@naver.com>
2026-04-24 15:04:48 +0900

    2026-04-24 15'04

---
+++ 1. Bronze/Bronze 4/[B4] 34236 숭고한에 어서오세요.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+
+using namespace std;
+
+int main()
+{
+    FastIO;
+
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    int result = a[n - 1] + (a[1] - a[0]);
+    cout << result << '\n';
+}
