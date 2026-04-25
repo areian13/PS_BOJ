51dfae0b7d71dbd5893b67904640f9377f08d6a3
Areian <iantyrho13@naver.com>
2026-04-25 22:10:32 +0900

    2026-04-25 22'10

---
+++ 1. Bronze/Bronze 5/[B5] 34297 Parking Pandemonium.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+
+using namespace std;
+
+int main()
+{
+    FastIO;
+
+    int m, k, c;
+    cin >> m >> k >> c;
+
+    int result = m * c;
+    cout << result << '\n';
+}
